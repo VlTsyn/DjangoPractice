13916e3e5a0f379ed490f0e8c0f8250ed0db1361
Vladimir Tsynbusov <vladimirtsynbusov@gmail.com>
2025-11-29 12:13:51 +0300

    add validation for model Product by name, description and price

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
+    """Модель для категорий товаров"""
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
 
@@ -15,6 +16,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    """Модель для товаров"""
     name = models.CharField(max_length=100, verbose_name='Название')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='images/', blank=True, verbose_name='Изображение')
@@ -27,6 +29,6 @@ class Product(models.Model):
         return f'{self.name} {self.category} {self.price}'
 
     class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
         ordering = ['name',]
